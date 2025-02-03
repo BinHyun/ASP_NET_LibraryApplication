800273cfabd5c647fdbed28aba6ad6eb0ae69d43
BinHyun <binhk1004@gmail.com>
2025-02-03 22:04:05 +0900

    Setting

---
+++ LibraryApplication/Context/LibraryDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using LibraryApplication.Models;
+
+namespace LibraryApplication.Context
+{
+    public class LibraryDb :DbContext
+    {
+        public LibraryDb (): base("name=dbname") { }
+        public DbSet<Book> Books { get; set; }
+    }
+}
