e684ddac88741dc67315ae11fe9855b6297d4dd8
BinHyun <binhk1004@gmail.com>
2025-02-09 21:12:25 +0900

    Modified DisplayName

--- LibraryApplication/Models/Book.cs
+++ LibraryApplication/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,10 +11,15 @@ namespace LibraryApplication.Models
     {
         [Key]
         public int Book_U { get; set; }
+        [DisplayName("제목")]
         public string Title { get; set; }
+        [DisplayName("저자")]
         public string Writer { get; set; }
+        [DisplayName("요약")]
         public string Summary { get; set; }
+        [DisplayName("출판사")]
         public string Publisher { get; set; }
+        [DisplayName("출판일")]
         public int Published_data { get; set; }
     }
 }
